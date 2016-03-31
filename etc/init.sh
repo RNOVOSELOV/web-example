2df5679bc646f695af560d13e380ee48f7e3a9a9
Roman novoselov <roman.novoselov@azimut.ru>
2016-03-31 13:55:21 +0300

    fuix

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -fr /home/nginx/sites-enabled/default
+sudo rm -fr /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
